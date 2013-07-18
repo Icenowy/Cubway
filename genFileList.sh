0d3faa91c9229555521cfcc811e71a55daaaac62
910JQK <v910JQK@gmail.com>
2013-07-18 14:58:09 +0800

    130718

--- genFileList.sh
+++ genFileList.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ls "$@" -Alih --time-style=full-iso|head -n 7|awk 'NR==1 {print "{\x22totalsize\x22:\x22"$2"\x22,\x22""files""\x22:["} NR!=1 {print "{\x22inode\x22:\x22"$1"\x22,\x22pm\x22:\x22"$2"\x22"",\x22links\x22:\x22"$3"\x22,\x22owner\x22:\x22"$4"\x22,\x22grp\x22:\x22"$5"\x22,\x22size\x22:\x22"$6"\x22,\x22""date""\x22:\x22"$7"\x22,\x22time\x22:\x22"$8"\x22,\x22timezone\x22:\x22"$9"\x22,\x22name\x22:\x22";for (i=10;i<=NF;i=i+1)printf $i;printf "\x22},"} END {printf "]}"}'|sed 's/,]}/]}/'|sed ':a;N;$!ba;s/\n//g'
+ls "$@" -AQlih --time-style=full-iso|awk 'NR==1 {print "{\x22totalsize\x22:\x22"$2"\x22,\x22""files""\x22:["} NR!=1 {print "{\x22inode\x22:\x22"$1"\x22,\x22pm\x22:\x22"$2"\x22"",\x22links\x22:\x22"$3"\x22,\x22owner\x22:\x22"$4"\x22,\x22grp\x22:\x22"$5"\x22,\x22size\x22:\x22"$6"\x22,\x22""date""\x22:\x22"$7"\x22,\x22time\x22:\x22"$8"\x22,\x22timezone\x22:\x22"$9"\x22,\x22name\x22:";for (i=10;i<=NF;i=i+1)printf $i;printf "\},"} END {printf "]}"}'|sed 's/\\"/\&quot\;/g;s/"->"/","linkto":"/g;s/</\&lt\;/g;s/>/\&gt\;/g;s/ /\&nbsp\;/g;s/,]}/]}/'|sed ':a;N;$!ba;s/\n//g'
 
 #"totalsize" doesn't include folders.
