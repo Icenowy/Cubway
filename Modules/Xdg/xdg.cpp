89bee58ed302837b1295a52a68be0a75192b9de6
910JQK <v910JQK@gmail.com>
2016-01-31 21:21:42 +0800

    Xdg: Add loadDesktopFile()

--- Modules/Xdg/xdg.cpp
+++ Modules/Xdg/xdg.cpp
@@ -1,12 +1,13 @@
 #include <QVariantMap>
 #include <qt5xdg/XdgDirs>
 #include <qt5xdg/XdgIcon>
+#include <qt5xdg/XdgDesktopFile>
 #include "xdg.h"
 
 
 QVariantMap XdgModule::getUserDirs(){
   QVariantMap result;
-  
+
   result["desktop"] = XdgDirs::userDir(XdgDirs::Desktop);
   result["download"] = XdgDirs::userDir(XdgDirs::Download);
   result["templates"] = XdgDirs::userDir(XdgDirs::Templates);
@@ -25,8 +26,52 @@ QVariantMap XdgModule::getUserDirs(){
 }
 
 
-QPixmap XdgModule::getIcon(const QString &name, int width, int height){
-  return XdgIcon::fromTheme(name).pixmap(width, height);
+QVariantMap XdgModule::loadDesktopFile(const QString &fileName){
+  QVariantMap result;
+
+  XdgDesktopFile desktop;
+  desktop.load(fileName);
+  switch(desktop.type()){
+  case XdgDesktopFile::UnknownType:
+    result["type"] = "unknown";
+    break;
+  case XdgDesktopFile::ApplicationType:
+    result["type"] = "application";
+    break;
+  case XdgDesktopFile::LinkType:
+    result["type"] = "link";
+    break;
+  case XdgDesktopFile::DirectoryType:
+    result["type"] = "directory";
+  }
+  result["valid"] = desktop.isValid();
+  result["categories"] = desktop.categories();
+  result["icon"] = desktop.icon().pixmap(icon_size, icon_size);
+  result["icon_name"] = desktop.iconName();
+  result["name"] = desktop.name();
+  result["comment"] = desktop.comment();
+  QString url = desktop.url();
+  QVariant exec = desktop.value("exec");
+  if(!url.isEmpty())
+    result["url"] = url;
+  if(exec.isValid())
+    result["exec"] = exec;
+  return result;
+}
+
+
+int XdgModule::getIconSize(){
+  return icon_size;
+}
+
+
+void XdgModule::setIconSize(int value){
+  icon_size = value;
+}
+
+
+QPixmap XdgModule::getIcon(const QString &name){
+  return XdgIcon::fromTheme(name).pixmap(icon_size, icon_size);
 }
 
 
--- Modules/Xdg/xdg.h
+++ Modules/Xdg/xdg.h
@@ -11,11 +11,17 @@ class XdgModule: public QObject
     Q_PLUGIN_METADATA(IID "org.subway.Cubway.Xdg")
     Q_PROPERTY(QVariantMap userDirs READ getUserDirs)
     Q_PROPERTY(QString themeName READ getThemeName WRITE setThemeName)
+    Q_PROPERTY(int iconSize READ getIconSize WRITE setIconSize)
 public:
     QVariantMap getUserDirs();
+    Q_INVOKABLE QVariantMap loadDesktopFile(const QString &fileName);
     QString getThemeName();
     void setThemeName(const QString &name);
-    Q_INVOKABLE QPixmap getIcon(const QString &name, int width, int height);
+    int icon_size = 16;
+    int getIconSize();
+    void setIconSize(int value);
+    Q_INVOKABLE QPixmap getIcon(const QString &name);
 };
+
 
 #endif //_MODULES_XDG_H
